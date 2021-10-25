f8c0f77fa89ef45818b70aba90e67a08edc31d98
Zeyang Lai <decyctus@gmail.com>
2021-10-24 21:19:47 -0700

    add an init function that will ask the users what they want to do after invoke the application

---
+++ index.js
@@ -0,0 +1,45 @@
+//Import and require inquirer,mysql2
+const inquirer=require("inquirer");
+const Choice = require("inquirer/lib/objects/choice");
+const mysql=require("mysql2");
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: 'hohoattack',
+      database: 'employee_db'
+    }
+  );
+
+// init function to ask the users what they want to do
+  function init(){
+      return inquirer.prompt([
+          {
+              type:"list",
+              message:"What would you like to do?",
+              name:"choose",
+              choices:["View all departments","View all roles","View all employees","Add a department","Add a role","Add an employee","Update an employee role"],
+          }
+      ]).then((data)=>{
+          switch(data.choose){
+              case "View all departments":
+                  return viewDepartment();
+              case "View all roles":
+                  return viewRoles();
+              case "View all employees":
+                  return viewEmployees();
+              case "Add a department":
+                  return addDepartment();
+              case "Add a role":
+                  return addRole();
+              case "Add an employee":
+                  return addEmployee();
+              case "Update an employee role":
+                  return updateEmployee();
+          }
+      });
+  }
